c5206bd132c494b432b42212dbc0b281c7ea0547
AndroViana <dev.johnandroviana@gmail.com>
2023-07-16 16:04:03 +0800

    test 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          John Andro Viana
+          John Andro Sample
         </a>
       </header>
     </div>
