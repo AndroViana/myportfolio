9503b6fb4ca11f05c660a2bc8ab89bbf7864e6eb
AndroViana <dev.johnandroviana@gmail.com>
2023-07-17 09:13:29 +0800

    deleted

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import About from "./pages/about";
-import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
 
 function App() {
@@ -14,8 +13,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="projects" element={<Projects />} />
           <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
+              <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
